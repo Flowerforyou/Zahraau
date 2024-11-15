68ab37a8cde4d653a4b1f5eeb835374a3fb1a8a6
Flowerforyou <160044628+Flowerforyou@users.noreply.github.com>
2024-11-15 23:03:04 +0700

    Add files via upload

---
+++ js/jav.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Pacarnya Arby", "Cewek lucu", "Tentu cantikk😍",
+     "Beautiful"],
+     typeSpeed: 100,
+     backspeed: 100,
+     backDelay: 1000,
+     loop: true
+});
